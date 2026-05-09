b3639c1709e41ae898a89c68521dffac5267176e
Deepak7447 <deepakdwivedi7249@gmail.com>
2026-05-09 22:39:30 +0530

    Added invoice app project

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express=require("express");
 const mongoose=require("mongoose");
+const Invoice=require("./models/invoice");
+
 const app=express();
 
 
@@ -15,6 +17,31 @@ app.get("/",(req,res)=>{
 res.send("Hello Worlds");
 });
 
+
+app.get("/create-test",async(req,res)=>{
+    const newInvoice=new Invoice({
+        customerName:"Deepak",
+        items:[
+            {
+                name:"mobile",
+                quantity:2,
+                price:30000
+            },
+            {
+                name:"laptop",
+                quantity:1,
+                price:50000
+            }
+        ],
+
+        subtotal:110000,
+        gst:12000,
+        total:122000
+    });
+
+    await newInvoice.save();
+    res.send("Test inovice created");
+})
 app.listen(3000,()=>{
 console.log("server is runnign");
 });
---
+++ models/invoice.js
@@ -0,0 +1,31 @@
+const mongoose=require("mongoose");
+
+const itemSchema=new mongoose.Schema({
+    name:String,
+    quantity:Number,
+    price:Number
+});
+
+const invoiceSchema=new mongoose.Schema({
+    customerName:{
+    type:String,
+    required:true
+    },
+
+    items:[itemSchema],
+    subtotal:Number,
+    gst:Number,
+    total:Number,
+
+    status:{
+        type:String,
+        enum:["Paid","Unpaid"],
+        default:"Unpaid"
+    },
+
+    createdAt:{
+        type:Date,
+        default:Date.now}
+});
+
+module.exports=mongoose.model("Invoice",invoiceSchema);
